22ace76f74a7b4a9c207599c10c14da5db9c323c
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-12-02 00:15:35 +0700

    Config redux

---
+++ src/redux/slices/sampleSlice.ts
@@ -0,0 +1,16 @@
+//This is template file
+import { createSlice } from "@reduxjs/toolkit"
+
+interface SliceType {
+
+}
+
+const initialState: SliceType = {
+
+}
+
+export const slice = createSlice({
+    name: '',
+    initialState,
+    reducers: {}
+})

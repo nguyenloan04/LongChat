b932480df6f9aa45a90bc7f2ee537dc97e6d15bf
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2026-01-02 14:44:00 +0700

    Fix ComingSoon Page (final)

--- src/components/comingsoon/ComingSoon.tsx
+++ src/components/comingsoon/ComingSoon.tsx
@@ -3,13 +3,24 @@ import {Button} from "@/components/ui/button.tsx";
 import {useNavigate} from "react-router-dom";
 
 export default function ComingSoon() {
+    const text = "Coming soon..."
     const navigate = useNavigate();
     return (
         <div className="min-h-screen bg-white flex flex-col items-center justify-center p-4 relative overflow-hidden">
 
             <div className="z-10 text-center">
-                <h1 className="text-6xl md:text-8xl font-black text-appchat-bluesky tracking-widest mb-6 fade-in">
-                    Coming Soon...
+                <h1 className="text-6xl md:text-8xl font-black text-appchat-bluesky tracking-widest mb-6">
+                    {text.split("").map((char, index) => (
+                        <span
+                            key={index}
+                            className="char-fade-in"
+                            style={{
+                                animationDelay: `${index * 0.1}s`
+                            }}
+                        >
+                            {char === " " ? "\u00A0" : char}
+          </span>
+                    ))}
                 </h1>
 
                 <motion.p

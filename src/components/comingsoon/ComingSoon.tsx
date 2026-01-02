0137c0eaad6e653cc83c9cfdf903ac20d5b9aed7
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2026-01-02 09:30:06 +0700

    Fix ComingSoon Page

--- src/components/comingsoon/ComingSoon.tsx
+++ src/components/comingsoon/ComingSoon.tsx
@@ -3,42 +3,14 @@ import {Button} from "@/components/ui/button.tsx";
 import {useNavigate} from "react-router-dom";
 
 export default function ComingSoon() {
-    const text = "Coming soon..."
-    const containerVariants = {
-        hidden: {opacity: 0},
-        visible: {
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.15
-            },
-        },
-    }
-    const letterVariants = {
-        hidden: {opacity: 0, y: 10},
-        visible: {
-            opacity: 1,
-            y: 0,
-            transition: {type: "spring", stiffness: 100}
-        },
-    }
     const navigate = useNavigate();
     return (
         <div className="min-h-screen bg-white flex flex-col items-center justify-center p-4 relative overflow-hidden">
 
             <div className="z-10 text-center">
-
-                <motion.h1
-                    className="text-5xl md:text-8xl font-black text-appchat-bluesky tracking-widest mb-6 font-sans"
-                    variants={containerVariants}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    {text.split("").map((char, index) => (
-                        <motion.span key={index} variants={letterVariants}>
-                            {char}
-                        </motion.span>
-                    ))}
-                </motion.h1>
+                <h1 className="text-6xl md:text-8xl font-black text-appchat-bluesky tracking-widest mb-6 fade-in">
+                    Coming Soon...
+                </h1>
 
                 <motion.p
                     className="text-gray-400 text-lg md:text-xl mb-8 max-w-lg mx-auto"
@@ -46,7 +18,7 @@ export default function ComingSoon() {
                     animate={{opacity: 1}}
                     transition={{delay: 2.5, duration: 1}}
                 >
-                    Chúng tôi sẽ phát triển chức năng này sau.
+                    Chức năng này đang được phát triển. Hãy chờ nhé!!!
                 </motion.p>
                 <motion.div
                     initial={{opacity: 0}}

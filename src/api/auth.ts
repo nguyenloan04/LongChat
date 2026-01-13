19a94886eabdfe8ce48e3532f95a72e998804436
Riichan04 <22130274@st.hcmuaf.edu.vn>
2026-01-14 01:29:57 +0700

    Complete auth api handler

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,19 +1,49 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+import axios from 'axios'
+dotenv.config()
 
-// const SERVER_URL = process.env.SERVER_URL
+const SERVER_URL = process.env.SERVER_URL
 
 export const authApi = new Map([
     {
-        name: "login",
-        handle: () => {
+        name: "get-description",
+        handle: async (username: string) => {
+            return await axios.get(`${SERVER_URL}/get-description?username=${username}`).then(response => {
+                try {
+                    const data = response.data
+                    return JSON.parse(data) as { result: boolean, description: string }
+                }
+                catch (_) {
+                    return { result: false, description: "" }
+                }
+            })
         }
     },
     {
         name: "register",
-        handle: () => {
-
+        handle: async (username: string) => {
+            return await axios.post(`${SERVER_URL}/create`, { username }).then(response => {
+                try {
+                    return JSON.parse(response.data) as { result: boolean }
+                }
+                catch (_) {
+                    return { result: false }
+                }
+            })
         }
     },
+    {
+        name: "update-description",
+        handle: async (username: string, description: string) => {
+            return await axios.post(`${SERVER_URL}/update-description`, { username, description }).then(response => {
+                try {
+                    return JSON.parse(response.data) as { result: boolean, description: string }
+                }
+                catch (_) {
+                    return { result: false, description: "" }
+                }
+            })
+        }
+    }
 ].map(e => [e.name, e.handle]))
 
